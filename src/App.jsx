4179ae2c32784f40e80cda5ef8b447ec22749bba
Carlos Grandis <carlosgrandis@gmail.com>
2024-09-17 13:55:59 -0300

    completed task 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ArticlesList from './Components/ArticlesList'
-import Header from './Components/Header'
+import ArticlesList from './Components/ArticlesList';
+import Header from './Components/Header';
 import ArticlePage from './Components/ArticlePage';
+import TopicsPage from './Components/TopicsPage';
+import TopicArticlesPage from './Components/TopicArticlesPage';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
           <Header />
         <Routes>
           <Route path="/" element={<ArticlesList />} />
+          <Route path="/topics" element={<TopicsPage />} />
+          <Route path='/topics/:topic' element={<TopicArticlesPage />} />
           <Route path="/articles/:article_id" element={<ArticlePage />} />
       </ Routes>
       </Router>
